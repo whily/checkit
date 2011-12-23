e709fcd993658827fb91bc475cc0cbf6bf179922
Yujian Zhang <yujian.zhang@gmail.com>
2011-12-23 16:34:28 +0800

    Add ListActivity.java (skeleton) and SettingsActivity.java.

---
+++ src/net/whily/android/checkit/SettingsActivity.java
@@ -0,0 +1,25 @@
+/**
+ * Settings activity for CheckIt.
+ *
+ * @author  Yujian Zhang <yujian{dot}zhang[at]gmail(dot)com>
+ *
+ * License: 
+ *   GNU General Public License v2
+ *   http://www.gnu.org/licenses/gpl-2.0.html
+ * Copyright (C) 2011 Yujian Zhang
+ */
+
+package net.whily.android.checkit;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class SettingsActivity extends PreferenceActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+        
+    // Load the preferences from an XML resource
+    addPreferencesFromResource(R.xml.settings);
+  }
+}
