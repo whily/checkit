47f886a74eaefa151ffacdf78b4bfbf91b17504d
Yujian Zhang <yujian.zhang@gmail.com>
2011-12-29 12:32:12 +0800

    Add input box to enter more items.

--- src/net/whily/android/checkit/CheckActivity.java
+++ src/net/whily/android/checkit/CheckActivity.java
@@ -28,7 +28,6 @@ import android.widget.CheckedTextView;
 public class CheckActivity extends ListActivity {
   private ArrayAdapter<String> arrayAdapter;
 
-  /** Called when the activity is first created. */
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
