530a39f323f9303346ffa7da305bbe097a02b29b
Yujian Zhang <yujian.zhang@gmail.com>
2011-12-29 11:14:25 +0800

    Rename main.xml to check.xml.

--- src/net/whily/android/checkit/CheckActivity.java
+++ src/net/whily/android/checkit/CheckActivity.java
@@ -32,7 +32,7 @@ public class CheckActivity extends ListActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.main);
+    setContentView(R.layout.check);
 
     String[] str = getResources().getStringArray(R.array.travel_list);
     arrayAdapter = new 
