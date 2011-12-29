06b8a5b08c82728318df6194eacae83820cce14c
Yujian Zhang <yujian.zhang@gmail.com>
2011-12-29 11:10:29 +0800

    Add context menus and use R.id for menu items.

--- src/net/whily/android/checkit/CheckActivity.java
+++ src/net/whily/android/checkit/CheckActivity.java
@@ -15,38 +15,41 @@ import android.app.ListActivity;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.CheckedTextView;
 
 public class CheckActivity extends ListActivity {
-  public static final int UNCHECK_ALL_ID = Menu.FIRST + 1;  
-  public static final int SETTINGS_ID = Menu.FIRST + 2;
-  public static final int ABOUT_ID = Menu.FIRST + 3;
-  
   private ArrayAdapter<String> arrayAdapter;
 
   /** Called when the activity is first created. */
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+
     String[] str = getResources().getStringArray(R.array.travel_list);
     arrayAdapter = new 
       ArrayAdapter<String>(this, android.R.layout.simple_list_item_checked, str);
+    ListView list = (ListView)getListView();
     setListAdapter(arrayAdapter);
-    setContentView(R.layout.main);
+
+    registerForContextMenu(list);
   }
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
-    menu.add(0, UNCHECK_ALL_ID, 0, R.string.uncheck_all)
+    menu.add(0, R.id.uncheck_all, 0, R.string.uncheck_all)
       .setIcon(android.R.drawable.ic_menu_close_clear_cancel);      
-    menu.add(0, SETTINGS_ID, 0, R.string.settings)
+    menu.add(0, R.id.settings, 0, R.string.settings)
       .setIcon(android.R.drawable.ic_menu_preferences);
-    menu.add(0, ABOUT_ID, 1, R.string.about)
+    menu.add(0, R.id.about, 1, R.string.about)
       .setIcon(android.R.drawable.ic_menu_info_details);
     return super.onCreateOptionsMenu(menu);
   }
@@ -54,15 +57,15 @@ public class CheckActivity extends ListActivity {
   @Override
   public boolean onOptionsItemSelected(MenuItem item) {
     switch(item.getItemId()) {
-      case UNCHECK_ALL_ID:
+      case R.id.uncheck_all:
         arrayAdapter.clear();
         return true;
         
-      case SETTINGS_ID:
+      case R.id.settings:
         startActivity(new Intent(this, SettingsActivity.class));
         return true;
 
-      case ABOUT_ID:
+      case R.id.about:
         startActivity(new Intent(this, AboutActivity.class));
         return true;
 
@@ -77,5 +80,34 @@ public class CheckActivity extends ListActivity {
     // FIXME: currently items are randomly selected/deselcted.
     CheckedTextView textView = (CheckedTextView)v;
     textView.setChecked(!textView.isChecked());
+  }
+
+  @Override
+  public void onCreateContextMenu(ContextMenu menu, View v,
+                                  ContextMenuInfo menuInfo) {
+    super.onCreateContextMenu(menu, v, menuInfo);
+    menu.add(Menu.NONE, R.id.edit, Menu.NONE, R.string.edit);
+    menu.add(Menu.NONE, R.id.delete, Menu.NONE, R.string.delete);
+  }
+
+  @Override
+  public boolean onContextItemSelected(MenuItem item) {
+    AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+    switch (item.getItemId()) {
+      case R.id.edit:
+        editItem(info.id);
+        return true;
+      case R.id.delete:
+        deleteItem(info.id);
+        return true;
+      default:
+        return super.onContextItemSelected(item);
+    }
+  }
+
+  private void editItem(long id) {
+  }
+
+  private void deleteItem(long id) {
   }
 }
